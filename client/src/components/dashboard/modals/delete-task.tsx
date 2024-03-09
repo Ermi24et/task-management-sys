93ecae1ed30b7577148f5df4676fb94adb0790ee
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-03-09 18:41:03 +0000

    fix: fix styling

--- client/src/components/dashboard/modals/delete-task.tsx
+++ client/src/components/dashboard/modals/delete-task.tsx
@@ -8,6 +8,7 @@ import CreateTaskForm from "@/components/dashboard/create-task-form";
 import {useTask} from "@/hooks/task";
 import {useTasks} from "@/contexts/useTask";
 import {useToast} from "@/components/ui/use-toast";
+import {Button} from "@/components/ui/button";
 
 interface ModalProps {
     open: boolean;
@@ -77,21 +78,21 @@ export default function DeleteTask({open, setOpen, id}: ModalProps): React.React
                                     </p>
                                 </div>
                                 <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                                    <button
+                                    <Button
                                         type="button"
                                         className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-1 lg:py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
                                         onClick={handleSubmit}
                                     >
                                         Delete
-                                    </button>
-                                    <button
+                                    </Button>
+                                    <Button
                                         type="button"
                                         className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-1 lg:py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
                                         onClick={() => setOpen(false)}
                                         ref={cancelButtonRef}
                                     >
                                         Cancel
-                                    </button>
+                                    </Button>
                                 </div>
                             </Dialog.Panel>
                         </Transition.Child>
