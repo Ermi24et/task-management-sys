ee7a2f59afead3941a7d2cd5dd4c2cd7b1f07016
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-03-10 14:02:21 +0000

    fix: fix types

--- client/src/lib/types/Task.ts
+++ client/src/lib/types/Task.ts
@@ -1,16 +1,18 @@
+import {User} from "@/lib/types/User";
+
 export interface Task {
     id?: string;
     title: string;
     description: string;
     status?: 'pending' | 'completed' | 'in_progress';
-    user?: string;
+    user?: User;
     createdAt?: string;
     updatedAt?: string;
     dueDate?: string | Date | undefined;
     due_date?: string | Date | undefined;
     priority?: 'low' | 'mid' | 'high';
-    assignedBy?: string;
-    assignedTo?: string;
+    assignedBy?: User;
+    assignedTo?: User;
 }
 
 export interface CreateTask extends Task {
